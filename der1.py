1746e32253d8ea86a8b1194ec1a8fd9ac8fd6c48
abhi1agarwal <abhi.agarwal281295@gmail.com>
2017-03-14 17:19:48 +0530

    07:58

--- der1.py
+++ der1.py
@@ -5,3 +5,28 @@ print("Hello World")
 #single and double quotes are exactly the same 
 name="abhinadan agarwal"
 print(name)
+# Numbers strings Lists Tuples Dictionaries 
+name = 15
+'''
+	+ - * / % **(expo) // (floor division)
+'''
+
+print (3//2)
+print (3/2)
+# OBSERVE THE DIFFERENCE BETWEEN THE TWO
+
+print("1+2-3*2=",1+2-3*2)
+#comma helps in concatenation 
+"asda"
+multi='''
+yaaro
+dosti  '''
+
+print("yuki " + multi)
+#above shows multi line strings 
+
+print("i dont like it ",end="") # gets rid of the new lines for once
+print("newlines")
+print("are there still new lines ?")
+
+
